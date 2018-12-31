74015d7d7e27f3bbf40127804323edc8f8479065
Christina Williams <christinaxtwilliams@gmail.com>
2018-12-31 02:00:23 +0000

    modified lib

--- lib/Niche-TS/school.rb
+++ lib/Niche-TS/school.rb
@@ -7,12 +7,12 @@ class Niche-TS::School
 	    @@all = []
 	
 
-	    def initialize(name=nil, location=nil, rank=nil, url=nil, cost=nil, acceptance_rate-nil)
+	    def initialize(name=nil, location=nil, rank=nil, url=nil, cost=nil, acceptance_rate=nil)
 	        @name = name
 	        @location = location
 	        @rank = rank
 	        @url = url
 	        @cost = cost
-                @acceptance_rate=acceptance_rate
+                @acceptance_rate = acceptance_rate
 	        @@all << self
 	    end
