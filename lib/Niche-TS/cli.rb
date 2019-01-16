0db1de620bba30c201999ff6a1396a6d0a5ce72b
Christina Williams <christinaxtwilliams@gmail.com>
2019-01-16 20:30:37 +0000

    modified lib

--- lib/Niche-TS/cli.rb
+++ lib/Niche-TS/cli.rb
@@ -10,15 +10,15 @@ class Niche-TS::CLI
     
     def run
         print_school_list
-        puts "===========================================" .blue
+        puts "===========================================" .colorize(:blue)
         puts "WANT TO KNOW ABOUT THE TOP COLLEGES FOR THEATRE?"
         puts "ENTER THE NUMBER OF THE THEATRE SCHOOL YOU WOULD LIKE TO GET MORE INFORMATION ON, OR TYPE 'EXIT' TO QUIT.  " 
-        puts "===========================================" .blue
+        puts "===========================================" .colorize(:blue)
 
         input = gets.strip  
         
         # Invalid input handling
-        while !(input == 'exit' || input.to_i >= 1 && input.to_i <= 30)
+        while !(input == 'exit' || input.to_i >= 1 && input.to_i <= 10)
             puts "Input was invalid. Please try again."
             input = gets.strip
         end
@@ -26,12 +26,12 @@ class Niche-TS::CLI
         if input != "exit"
 	            print_school_detail(Niche-TS::School.find_by_rank(input))
 	            
-	            puts "Type 'back' to return to college list, or 'exit' to quit."
+	            puts "Type 'back' to return to college list, or 'exit' to quit.".colorize(:yellow)
 	             input_2 = gets.strip  ##INPUT
 	            
 	            # Invalid input handling
 	      while !(input_2 == 'exit' || input_2 == 'back')
-	            puts "Input was invalid. Please try again."
+	            puts "Input was invalid. Please try again.".colorize(:red)
 	             input_2 = gets.strip
 	      end
 	            
@@ -43,7 +43,7 @@ class Niche-TS::CLI
 	    
 	    def print_school_list
 	        puts ""
-	        puts "------ Niche.com 2019 Best Colleges for Performing Arts in America ------"
+	        puts "------ Niche.com 2019 Best Colleges for Performing Arts in America ------".colorize(:yellow)
 	        puts ""
 	
 
@@ -58,14 +58,16 @@ class Niche-TS::CLI
 
 	    def print_school_detail(school)
 	        puts ""
+	        print "__________________________".colorize(:blue)
 	        print " #{school.name}, #{school.location} "
+	        print "__________________________".colorize(:blue)
 	        puts ""
-	        puts "Rank: " + "#{school.rank}"
+	        puts "Rank: ".colorize(:blue) + "#{school.rank}"
 	        puts ""
-	        puts "Acceptance Rate: " + "#{school.acceptance_rate"
+	        puts "Acceptance Rate: ".colorize(:blue) + "#{school.acceptance_rate"
 	        puts ""
-	        puts "Average Annual Cost: " + "#{school.cost}”
-	        puts "School Website: " + "#{school.url}"
+	        puts "Average Annual Cost: ".colorize(:blue) + "#{school.cost}”
+	        puts "School Website: ".colorize(:blue) + "#{school.url}"
 	        
 	   end
 	 end
