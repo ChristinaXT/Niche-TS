8a1872d6346c5288a903f58c59b543781746a7ca
Xtina <Xtina@owners-MacBook-Air.local>
2019-02-08 11:56:59 -0500

    corrected scrape method

--- lib/Niche_TS/cli.rb
+++ lib/Niche_TS/cli.rb
@@ -1,60 +1,61 @@
 require 'colorize'
 
 class Niche_TS::CLI
-  
+
   def call
         Niche_TS::Scraper.new.create_schools
 
         run
     end
-    
+
     def run
         print_school_list
         puts "===========================================" .colorize(:blue)
         puts "WANT TO KNOW ABOUT THE TOP 10 COLLEGES FOR THEATRE?"
-        puts "ENTER THE NUMBER OF THE THEATRE SCHOOL BY RANK YOU WOULD LIKE TO GET MORE INFORMATION ON, OR TYPE 'EXIT' TO QUIT.  " 
+        puts "ENTER THE NUMBER OF THE THEATRE SCHOOL BY RANK YOU WOULD LIKE TO GET MORE INFORMATION ON, OR TYPE 'EXIT' TO QUIT.  "
         puts "===========================================" .colorize(:blue)
 
-        input = gets.strip  
-        
+        input = gets.strip
+
         # Invalid input handling
         while !(input == 'exit' || input.to_i >= 1 && input.to_i <= 10)
             puts "Input was invalid. Please try again."
             input = gets.strip
         end
-        
+
         if input != "exit"
-	            print_school_detail(Niche_TS::School.find_by_rank(input))
-	            
+          school = Niche_TS::School.find_by_rank(input)
+              print_school_detail(Niche_TS::School.find_by_rank(input))
+
 	            puts "Type 'back' to return to college list, or 'exit' to quit.".colorize(:yellow)
 	             input_2 = gets.strip  ##INPUT
-	            
+
 	            # Invalid input handling
 	      while !(input_2 == 'exit' || input_2 == 'back')
 	            puts "Input was invalid. Please try again.".colorize(:red)
 	             input_2 = gets.strip
 	      end
-	            
+
 	      if input_2 == "back"
 	         run
 	      end
 	   end
 	 end
-	    
+
 	    def print_school_list
 	        puts ""
 	        puts "------ Niche.com 2019 Best Colleges for Performing Arts in America ------".colorize(:yellow)
 	        puts ""
-	
+
 
          Niche_TS::School.all.each do |school|
 	       puts "  #{school.rank} " + " #{school.name}, #{school.location}"
 	   end
-	
+
 
 	        puts ""
 	    end
-	
+
 
 	    def print_school_detail(school)
 	        puts ""
@@ -66,11 +67,8 @@ class Niche_TS::CLI
 	        puts ""
 	        puts "Acceptance Rate: ".colorize(:blue) + "#{school.acceptance_rate}"
 	        puts ""
-	        puts "Average Annual Cost: ".colorize(:blue) + "#{school.cost}”
+	        puts "Average Annual Cost: ".colorize(:blue) + "#{school.cost}"
 	        puts "School Website: ".colorize(:blue) + "#{school.url}"
-	        
+
 	   end
 	 end
-	        
-	        
-	       
--- lib/Niche_TS/scraper.rb
+++ lib/Niche_TS/scraper.rb
@@ -1,14 +1,18 @@
 class Niche_TS::Scraper
-	
+
 
 	    def get_page
 	        doc = Nokogiri::HTML(open("https://www.niche.com/colleges/search/best-colleges-for-theater/"))
 	    end
-	    
+
 	    def scrape_schools_index
 	      self.get_page.css ("#maincontent > div > div > section > div.search-results > ol")
 	    end
-	    
+
+			def scrape_schools_details
+				#self.get_page.css
+			end
+
 	    def create_schools
         scrape_schools_index.each do |school_xml|
             Niche_TS::School.new_from_index_xml_page(school_xml)
@@ -16,4 +20,3 @@ class Niche_TS::Scraper
         Niche_TS::School.all
     end
 end
-	    
